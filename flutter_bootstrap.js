f73d695a7ad9ad1127930fd998f11fa2d463c11d
DutkinTE <dutkinte@gmail.com>
2024-05-08 23:08:31 +0300

    fix scroll

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"0734f47f2a3df93a92f33c60db717522597e89
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2074127145"
+    serviceWorkerVersion: "3462453022"
   }
 });
